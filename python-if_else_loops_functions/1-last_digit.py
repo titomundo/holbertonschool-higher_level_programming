2f2bcde7caa7d844ed8923b9aa7de542f193761c
titomundo <tito.alberto.mundo@gmail.com>
2026-01-16 23:46:48 -0600

    change string format

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
 
-print(f"The last digit of {number} is {last} and is ", end="")
+print(f"Last digit of {number} is {last} and is ", end="")
 
 if last > 5:
     print("greater than 5")
