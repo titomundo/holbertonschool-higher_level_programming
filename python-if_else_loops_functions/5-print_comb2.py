0b0709daba8cc85f638663875cb6a82313179585
titomundo <tito.alberto.mundo@gmail.com>
2026-01-17 09:17:44 -0600

    use less than 2 print calls

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print("{0:02d}".format(i), end="")
-
-    if i < 99:
-        print(", ", end="")
-    else:
-        print()
+    print("{0:02d}".format(i), end=", " if i < 99 else '\n')
