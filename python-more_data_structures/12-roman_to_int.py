fff86bf508947a61881ac63d53b67452ae6325cb
titomundo <tito.alberto.mundo@gmail.com>
2026-01-23 18:33:53 -0600

    check for no string cases

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,9 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number, prev = 0, 1000
 
+    if not roman_string or type(roman_string) is not str:
+        return 0
+
     for i in range(len(roman_string)):
         cur = roman.get(roman_string[i])
 
