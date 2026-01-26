f8c2d52049007d4b75239a7280f7ef0c08077a13
titomundo <tito.alberto.mundo@gmail.com>
2026-01-25 22:30:43 -0600

    task 0 exceptions

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+
+        except IndexError:
+            i -= 1
+            break
+    print()
+    return i + 1
