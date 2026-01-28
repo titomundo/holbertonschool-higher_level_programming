2276b6c434fa3ba29048dfd2393a5be0d2fce621
titomundo <tito.alberto.mundo@gmail.com>
2026-01-28 14:51:22 -0600

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+''' Demo Square class module '''
+
+
+class Square:
+    ''' Square Class
+    Squares have four sides of equal length '''
+    def __init__(self, size):
+        self._size = size
