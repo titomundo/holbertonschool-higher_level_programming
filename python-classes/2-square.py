bf9b40d822ede2e42286cb5575ab92c9fcaa5a9f
titomundo <tito.alberto.mundo@gmail.com>
2026-01-28 16:23:58 -0600

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+''' Demo Square class module '''
+
+
+class Square:
+    ''' Square Class
+    Squares have four sides of equal length '''
+
+    def __init__(self, size=0):
+        ''' Square constructor
+        Enables to give parameters such as size
+
+        Args:
+            size (int): Size of the square
+        '''
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
