16e623972f6d5c18cad64c1ce86c9b58e11f445e
titomundo <tito.alberto.mundo@gmail.com>
2026-01-28 15:47:35 -0600

    docstring for init

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,12 @@
 class Square:
     ''' Square Class
     Squares have four sides of equal length '''
+
     def __init__(self, size):
+        ''' Square constructor
+        Enables to give parameters such as size
+
+        Args:
+            size (int): Size of the square
+        '''
         self._size = size
