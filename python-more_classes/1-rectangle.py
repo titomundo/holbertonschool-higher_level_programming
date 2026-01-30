f5ffd13653d9cfce3c95450d387e54f44220fe0c
titomundo <tito.alberto.mundo@gmail.com>
2026-01-29 22:04:46 -0600

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""Rectangle Class Module example in Python"""
+
+
+class Rectangle:
+    """Rectangle class
+    Rectangles have four sides and four right angles"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle constructor
+        Args:
+            width (int): width of the square
+            height (int): height of the square"""
+
+        if not isinstance(width, int) or not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        elif width < 0 or height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        elif height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = height
