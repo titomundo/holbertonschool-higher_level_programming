e0be5339bdc47e3c45581175ad04aba81cffd920
titomundo <tito.alberto.mundo@gmail.com>
2026-02-05 14:24:24 -0600

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Base Geometry Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry: defines a geometrical shape"""
+
+    def area(self):
+        """area: currently not implemented"""
+
+        raise Exception("area() is not implemented")
