28d2db8769a50c6308c3f97794084bf515df861c
titomundo <tito.alberto.mundo@gmail.com>
2026-02-05 23:25:20 -0600

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""10-square: Declares the Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class, based on Rectangle class"""
+
+    def __init__(self, size):
+        """square declaration
+        Args:
+            size (int): size of the square's sides"""
+
+        try:
+            self.integer_validator("size", size)
+        except Exception as Err:
+            raise Err
+        else:
+            self.__size = size
+            super().__init__(size, size)
+
+    def __str__(self):
+        """returns user friendly information for rectangle"""
+        return f"[Square] {self.__size}/{self.__size}"
+
+    def area(self):
+        """returns the area of the square"""
+
+        return self.__size * self.__size
