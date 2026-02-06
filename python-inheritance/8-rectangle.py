9c406f3461dfba048aeb25bc199afdc666f2060f
titomundo <tito.alberto.mundo@gmail.com>
2026-02-05 22:51:41 -0600

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        try:
+            self.integer_validator("height", height)
+            self.integer_validator("width", width)
+        except Exception as Err:
+            raise Err
+        else:
+            self.__width = width
+            self.__height = height
