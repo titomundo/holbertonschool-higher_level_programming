ddcef542754ea474dcb4abec456a99eb71458003
titomundo <tito.alberto.mundo@gmail.com>
2026-02-05 22:06:55 -0600

    no checar herencias en la comparación de tipo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -17,7 +17,7 @@ class BaseGeometry:
             name (string): name of the value
             value (integer): value to evaluate"""
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
