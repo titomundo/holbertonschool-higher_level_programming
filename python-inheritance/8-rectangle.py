c718e03ef69836e8c8e850591afb238464107984
titomundo <tito.alberto.mundo@gmail.com>
2026-02-05 22:58:11 -0600

    fix order for module documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-""" Rectangle class, based on BaseGeometry"""
+"""7-base_geometry: Rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class, base on BaseGeomerty"""
+
     def __init__(self, width, height):
         try:
             self.integer_validator("height", height)
