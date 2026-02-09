f94b6842c1dca01a4238abbe6719636a9e40440e
titomundo <tito.alberto.mundo@gmail.com>
2026-02-08 21:09:37 -0600

    extend documentation of functions

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -7,19 +7,25 @@ class VerboseList(list):
     """Base VerboseList class"""
 
     def append(self, object):
-        """append method: override append method from list class"""
+        """append method: override append method from list class
+        Args:
+            object (object) element to add to the list"""
 
         super().append(object)
         print(f"Added [{object}] to the list.")
 
     def extend(self, iterable):
-        """extend method: override extend method from list class"""
+        """extend method: override extend method from list class
+        Args:
+            iterable (iterable object): iterable to add to the list"""
 
         super().extend(iterable)
         print(f"Extended the list with [{len(iterable)}] items.")
 
     def remove(self, value):
-        """remove method: override remove method from list class"""
+        """remove method: override remove method from list class
+        Args:
+            value (int): value to remove from the list"""
 
         try:
             super().remove(value)
@@ -28,7 +34,9 @@ class VerboseList(list):
             raise err
 
     def pop(self, pos=-1):
-        """pop method: override pop method from list class"""
+        """pop method: override pop method from list class
+        Args:
+            pos (int): index of the element to pop"""
 
         try:
             p = super().pop(pos)
