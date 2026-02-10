15550e13a2b9a8c5c47e1cb63d9dd8b633b72dfc
titomundo <tito.alberto.mundo@gmail.com>
2026-02-10 13:39:13 -0600

    document function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,11 @@ from json import dump
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file: writes an object to a text file in json representation
+    Args:
+        my_obj (object): object to write to file
+        filename (str): name of the file to write to
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
         dump(my_obj, f)
