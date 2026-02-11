97f4da8f81192cfc2a98a20ca0f7bcecd6c72054
titomundo <tito.alberto.mundo@gmail.com>
2026-02-10 21:54:33 -0600

    document function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,4 +6,7 @@ Return the dictionary description of a class for a JSON object
 
 
 def class_to_json(obj):
+    """class_to_json:
+    returns a dictionary for a json object
+    """
     return obj.__dict__
