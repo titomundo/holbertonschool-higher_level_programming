4532d0bc6b84fc67565545735722f8c07c1a1af2
titomundo <tito.alberto.mundo@gmail.com>
2026-02-18 13:39:02 -0600

    task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""task_02_requests:
+Use the requests module in python to fetch and store data from an API
+"""
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts/")
+    print(f"Status Code: {r.status_code}")
+    jdata = r.json()
+
+    for entry in jdata:
+        print(entry["title"])
+
+
+r = requests.get("https://jsonplaceholder.typicode.com/posts/")
+
+with open("posts.csv", "w", encoding="utf-8", newline="") as csvfile:
+    jdata = r.json()
+    headers = ["id", "title", "body"]
+    writer = csv.DictWriter(csvfile, headers)
+
+    writer.writeheader()
+
+    for entry in jdata:
+        entry.pop("userId")
+        writer.writerow(entry)
