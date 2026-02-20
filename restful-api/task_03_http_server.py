e08637d6feb8571f60d0a5a3d0f44035e7df49c0
titomundo <tito.alberto.mundo@gmail.com>
2026-02-19 18:08:35 -0600

    fix ending headers before adding contenttype in root entry

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -22,8 +22,8 @@ class SimpleServer(http.server.BaseHTTPRequestHandler):
 
         if path == "/":
             self.send_response(200)
-            self.end_headers()
             self.send_header("Content-Type", "text/plain")
+            self.end_headers()
             self.wfile.write(bytes("Hello, this is a simple API!\n", "utf-8"))
         elif path == "/data":
             self.send_response(200)
@@ -31,12 +31,12 @@ class SimpleServer(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(self.data).encode("utf-8") + b"\n")
         elif path == "/status":
-            self.send_response(200, "OK")
+            self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(bytes("OK\n", "utf-8"))
         else:
-            self.send_response(404, "Not Found")
+            self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(bytes("Endpoint not found\n", "utf-8"))
