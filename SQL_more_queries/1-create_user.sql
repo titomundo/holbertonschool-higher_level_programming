a92524a478577ab7751f5c243ec2577891b1d08b
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 14:41:47 -0600

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- 1-create_user
+-- Creates an user with admin privileges
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED WITH 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* 
+TO user_0d_1@localhost;
