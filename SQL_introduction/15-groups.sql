ba9e2689d93d1083c0ba4acf81ffd90c276087ad
titomundo <tito.alberto.mundo@gmail.com>
2026-03-04 22:45:21 -0600

    order in descending order

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
 -- 15-groups
 -- Calculates the number of times a same score appears in the table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number 
+FROM second_table 
+GROUP BY score 
+ORDER BY score DESC;
