5dd0fdb963860598cd5dcf5b9eee86f3be5d1de6
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 17:36:34 -0600

    task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- 8-cities_of_california_subquery
+-- Returns all the cities of california found in the database
+SELECT id, name 
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id ASC;
