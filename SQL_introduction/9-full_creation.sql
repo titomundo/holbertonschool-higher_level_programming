f527120aa78fb1497dea95cd94d0898ef3da1a3f
titomundo <tito.alberto.mundo@gmail.com>
2026-03-04 21:14:27 -0600

    task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- 9-full_creation
+-- Creates a table and adds data to the rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+INSERT INTO second_table VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
