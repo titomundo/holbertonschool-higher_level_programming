0bc315de38ea0bc741fc0cfa78f6f4726c50acce
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 20:24:41 -0600

    user inner join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Lists all the genres in the database with the total of shows linked to that genre
 SELECT name as genre, COUNT(id) as number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON id = genre_id
 GROUP BY name
 ORDER BY number_of_shows DESC;
