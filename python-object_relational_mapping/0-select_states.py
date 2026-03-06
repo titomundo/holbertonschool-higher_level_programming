a18471ca28f81ef59c747a1093f023e592198032
titomundo <tito.alberto.mundo@gmail.com>
2026-03-06 14:28:37 -0600

    fix first line

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
 
 """0-select_states
 Uses the MySQLdb module to interact with a database using Python
@@ -12,7 +12,8 @@ def db_connection():
     """Connects to the database and fetches all rows from the states table"""
 
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3]
+        host="localhost", port=3306,
+        user=argv[1], password=argv[2], database=argv[3]
     )
     c = db.cursor()
 
