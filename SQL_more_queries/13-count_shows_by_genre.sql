3f585594d4d4873eb3d4434f849aeadd4613b5c6
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 20:27:35 -0600

    use capital letters for AS

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 13-count_shows_by_genre
 -- Lists all the genres in the database with the total of shows linked to that genre
-SELECT name as genre, COUNT(show_id) as number_of_shows
+SELECT name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON id = genre_id
