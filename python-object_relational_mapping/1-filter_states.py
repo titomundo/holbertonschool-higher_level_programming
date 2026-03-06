5b6af345750a8f0d6658896e105e917a9dba1e9a
titomundo <tito.alberto.mundo@gmail.com>
2026-03-06 15:18:25 -0600

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""1-filter_states
+Uses the MySQLdb module to search in the database states that start with N
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def db_connection():
+    """Connects to the database and allows to fetch data"""
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], password=argv[2], database=argv[3]
+    )
+    c = db.cursor()
+
+    c.execute("""SELECT * FROM states WHERE name LIKE 'N%'""")
+    result = c.fetchall()
+
+    for i in result:
+        print(i)
+
+
+if __name__ == "__main__":
+    db_connection()
