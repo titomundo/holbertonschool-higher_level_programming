e8f32d1d631f9e8a518e54b429071e16a33eb862
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 20:26:38 -0600

    count by show id

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 13-count_shows_by_genre
 -- Lists all the genres in the database with the total of shows linked to that genre
-SELECT name as genre, COUNT(id) as number_of_shows
+SELECT name as genre, COUNT(show_id) as number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON id = genre_id
