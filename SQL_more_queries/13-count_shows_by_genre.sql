83161cd72d4d4ab08526a631e0cafcfc242fcc4d
titomundo <tito.alberto.mundo@gmail.com>
2026-03-05 20:10:51 -0600

    change name of second column to number_of_shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 13-count_shows_by_genre
 -- Lists all the genres in the database with the total of shows linked to that genre
-SELECT name as genre, COUNT(genre_id) as number_of_rows
+SELECT name as genre, COUNT(genre_id) as number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON id = genre_id
