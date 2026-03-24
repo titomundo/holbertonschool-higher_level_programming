cd308e1d1936b4ce695a04063f7f0141ddf3c59b
titomundo <tito.alberto.mundo@gmail.com>
2026-03-24 14:41:42 -0600

    task 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/Node
+
+const arg = process.argv[2];
+const parsed = parseInt(arg, 10);
+
+if (isNaN(parsed)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parsed);
+}
