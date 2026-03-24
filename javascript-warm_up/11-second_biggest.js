90d163d8a447a7dc1b0c543427d619255d0656b5
titomundo <tito.alberto.mundo@gmail.com>
2026-03-24 16:47:49 -0600

    task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv;
+args.splice(0, 2);
+
+const list = args.map((n) => {
+  return parseInt(n, 10);
+});
+
+list.sort((a, b) => {
+  return a - b;
+});
+
+if (list.length < 2) {
+  console.log(1);
+} else {
+  console.log(list[list.length - 2]);
+}
