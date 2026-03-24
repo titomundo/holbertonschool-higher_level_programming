75f2d6af338e93c6626121c524b1c5b81a8fe84e
titomundo <tito.alberto.mundo@gmail.com>
2026-03-24 16:03:07 -0600

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n === 1 || n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
